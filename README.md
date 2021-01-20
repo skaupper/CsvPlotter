4dfa8c03e3f64899099aaa297109488655484d11
Sebastian Kaupper <kauppersebastian@gmail.com>
2021-01-20 11:00:07 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# CsvPlotter
+
+A simple to use script to plot CSV files using [matplotlib](https://matplotlib.org/).
+
+
+## Current Features
+
+- Print all column headers.
+- Selectively plot columns. . This allows plotting any combination of columns.
+- Reducing the amount of plotted samples by taking every n-th sample or specifying a range of samples to plot.
+
+
+## (Maybe) Future Features
+
+- Plotting different columns with an alternate Y-Axis.
+- Multiple subplots in a single plot.
+- Saving and restoring plot configurations in e.g. a YAML file for an easy reuse of common plotting setups.
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='CsvPlotter-skaupper',
+    version='0.0.1',
+    author='Sebastian Kaupper',
+    author_email='kauppersebastian@gmail.com',
+    description='A simple to use CSV plotter',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/skaupper/CsvPlotter',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+
+    entry_points={
+        'console_scripts': [
+            'plot=CsvPlotter.plot:run'
+        ]
+    },
+    install_requires=[
+        'numpy>=1.15.0',
+        'matplotlib>=3.0.0'
+    ]
+)
