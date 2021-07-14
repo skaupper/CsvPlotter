eb717e427e092b421af884ca5bf7399d0951b183
Sebastian Kaupper <kauppsebastian@gmail.com>
2021-07-14 13:18:01 +0200

    for column metrics include the sum and the size of the dataset

--- CsvPlotter/internal/utilities/calc_metrics.py
+++ CsvPlotter/internal/utilities/calc_metrics.py
@@ -4,9 +4,11 @@ from .. import csv_handling
 def calc_metrics(input_file: str):
     data_obj = csv_handling.CsvData.from_file(input_file)
     for h in data_obj.headers:
+        d = data_obj.data[h][:data_obj.size]
         print('Column "{}"'.format(h))
-        print('    Min: {}'.format(min(data_obj.data[h])))
-        print('    Max: {}'.format(max(data_obj.data[h])))
-        print('    Avg: {}'.format(
-            sum(data_obj.data[h][:data_obj.size]) / data_obj.size))
+        print('    Min:  {}'.format(min(d)))
+        print('    Max:  {}'.format(max(d)))
+        print('    Sum:  {}'.format(sum(d)))
+        print('    Size: {}'.format(data_obj.size))
+        print('    Avg:  {}'.format(sum(d) / data_obj.size))
         print()
